ec3afe3b4bd3f9919fbb4f80420041fc026c7029
Aira Sakuranomiya <aira@aira.cafe>
2023-07-23 22:23:58 +0800

    完善主页

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,17 +5,43 @@ import { RouterLink } from 'vue-router'
 <template>
   <main>
     <nav>
-      <RouterLink to="/main">Main</RouterLink>
+      <RouterLink to="/main">主榜</RouterLink>
+      <RouterLink to="/extra">副榜</RouterLink>
     </nav>
   </main>
 </template>
 
 <style lang="scss" scoped>
-  nav {
-    display: flex;
+nav {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+  width: 100vw;
+  height: 100vh;
+}
+
+nav a {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 20px;
+  font-size: 1rem;
+  text-decoration: none;
+  background-color: hsla(var(--c-otmink-h), var(--c-otmink-s), var(--c-otmink-l), 1);
+  border-radius: 4px;
+  color: white;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  transition: 0.2s;
+
+  &:hover {
+    background-color: hsla(var(--c-otmink-h), var(--c-otmink-s), calc( var(--c-otmink-l) + 5% ), 1);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
   }
 
-  nav a {
-    font-size: 1rem;
+  &:active {
+    background-color: hsla(var(--c-otmink-h), var(--c-otmink-s), calc( var(--c-otmink-l) - 5% ), 1);
+    box-shadow: 0 0 0 rgba(0, 0, 0, 0.2);
   }
+}
 </style>
