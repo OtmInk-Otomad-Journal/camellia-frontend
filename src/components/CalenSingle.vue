fdb703d9274201ca371845e26376728cacbba8e0
Aira Sakuranomiya <aira@aira.cafe>
2026-09-05 17:43:08 +0800

    Update CalenSingle.vue

--- src/components/CalenSingle.vue
+++ src/components/CalenSingle.vue
@@ -13,8 +13,15 @@ defineProps({ data: Object })
       <div class="ca-title" v-html="data.title"></div>
       <div class="ca-subtitle">{{ data.subtitle }}</div>
     </div>
-    <img class="cover" :src="data.cover" alt="" />
-    <!-- <div class="cover-border" /> -->
+    <div class="ca-cover-frame">
+      <img class="cover" :src="data.cover" alt="" />
+      <div class="ca-cover-decor" aria-hidden="true">
+        <i class="ca-cover-corner ca-cover-corner--start"></i>
+        <i class="ca-cover-corner ca-cover-corner--end"></i>
+        <span class="ca-cover-rule"></span>
+        <span class="ca-cover-plus"></span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,6 +30,7 @@ defineProps({ data: Object })
   position: relative;
   width: 1765px;
   height: 257px;
+  will-change: transform;
 }
 
 .ca-time {
@@ -91,31 +99,95 @@ defineProps({ data: Object })
   max-width: 520px;
   font-size: 36px;
   font-weight: 400;
-  letter-spacing: -2.88px;
+  letter-spacing: -2px;
 }
 
-.cover {
+.ca-cover-frame {
   position: absolute;
   top: 0;
   left: 988px;
   width: 823px;
   height: 217px;
+}
+
+.ca-cover-frame .cover {
+  position: absolute;
+  inset: 0;
+  width: 100%;
+  height: 100%;
   overflow: hidden;
   object-fit: cover;
-  // clip-path: polygon(4% 0, 100% 0, 100% 100%, 0 100%);
   background: #212121;
   z-index: 1;
 }
 
-// .cover-border {
-//   position: absolute;
-//   top: 0;
-//   left: 974px;
-//   width: 823px;
-//   height: 217px;
-//   background-color: v-bind('data.color');
-//   // clip-path: polygon(4% 0, 100% 0, 100% 100%, 0 100%);
-// }
+.ca-cover-decor {
+  position: absolute;
+  inset: 0;
+  z-index: 2;
+  pointer-events: none;
+  color: #212121;
+}
+
+.ca-cover-corner {
+  position: absolute;
+  width: 28px;
+  height: 28px;
+  border-color: v-bind('data.color');
+  border-style: solid;
+}
+
+.ca-cover-corner--start {
+  top: -7px;
+  left: -7px;
+  border-width: 3px 0 0 3px;
+}
+
+.ca-cover-corner--end {
+  right: -7px;
+  bottom: -7px;
+  border-width: 0 3px 3px 0;
+}
+
+.ca-cover-rule {
+  position: absolute;
+  top: calc(100% + 15px);
+  right: 24px;
+  left: 0;
+  height: 1px;
+  background: currentColor;
+  opacity: 0.16;
+}
+
+.ca-cover-plus {
+  position: absolute;
+  top: calc(100% + 9px);
+  right: 0;
+  width: 13px;
+  height: 13px;
+  opacity: 0.4;
+
+  &::before,
+  &::after {
+    content: '';
+    position: absolute;
+    background: currentColor;
+  }
+
+  &::before {
+    top: 6px;
+    left: 0;
+    width: 13px;
+    height: 1px;
+  }
+
+  &::after {
+    top: 0;
+    left: 6px;
+    width: 1px;
+    height: 13px;
+  }
+}
 
 .timeline-dot {
   position: absolute;
